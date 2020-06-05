ab79b31c4170b5d007a242356b5af31e80831d20
kk4das <66222670+kk4das@users.noreply.github.com>
2020-06-05 10:29:43 -0400

    Compile speedup
    Changed SSB_TFT_Display from .ino to .cpp - speeds up compile - the compiler caches .cpp file builds
    Added function prototypes to SSB_TFT_Display.h  - required now that that that has changed
    Fixed variable names, cleaned up some documentation

--- SSB_TFT_Display.h
+++ SSB_TFT_Display.h
@@ -1,3 +1,6 @@
+#ifndef SSB_TFT_Display
+#define SSB_TFT_Display
+
 /*
  * SSB_TFT_Display.h
  * KK4DAS, Dean Souleles, KK4DAS@gmail.com
@@ -8,6 +11,7 @@
  * 
  */
 
+// ===========================Symbolic Constants==================================
 #define VFOA 0 
 #define VFOB 1
 
@@ -16,3 +20,27 @@
 
 #define RX 0
 #define TX 1
+
+// ===========================Function Prototypes==================================
+void displayClearScreen();
+void displayPrintat(String s, int x, int y, int fontsize, int color);
+void displayPrintat(int i, int x, int y, int fontsize, int color);
+void displayDrawBoundingBox(int len, int x, int y, int fontsize, int fillcolor);
+void displayDrawTextBox(String s, int x, int y, int fontsize, int color, int fillcolor);
+void displaySMeter(int level);
+void displayBanner(String s);
+void displayActVFO(uint32_t freq);
+void displayAltVFO(uint32_t freq);
+void displayVFOAB(int vfo);
+void displayTxRx(int tx_rx);
+void displayMode(int mode);
+void displayIncr(int increment);
+void displaySetup(String banner,
+                  uint32_t vfoActfreq, uint32_t vfoAltfreq,
+                  uint32_t activeVFO,
+                  int tx_rx,
+                  int sideband,
+                  int increment,
+                  int s_meter);
+                  
+#endif
